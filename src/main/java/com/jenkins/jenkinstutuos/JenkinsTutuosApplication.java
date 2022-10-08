5ae84a60b82dc11adf682fc057bde1799e088e65
aibrghoutn <abdellatif.ibrghoutn@smart-rx.com>
2022-10-08 14:08:30 +0100

    Initial commit

---
+++ src/main/java/com/jenkins/jenkinstutuos/JenkinsTutuosApplication.java
@@ -0,0 +1,13 @@
+package com.jenkins.jenkinstutuos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsTutuosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsTutuosApplication.class, args);
+    }
+
+}
